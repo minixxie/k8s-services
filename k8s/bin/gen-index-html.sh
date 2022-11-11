0718c0e740c58c6dcd58f070f008e77c9cafa1a9
Simon Xie <simon.xie@kuulabu.com>
2022-11-11 15:05:44 +0800

    Fixed wrong command

--- k8s/bin/gen-index-html.sh
+++ k8s/bin/gen-index-html.sh
@@ -6,7 +6,7 @@ tmp="$tmp".html
 
 grafanaPwd=$(kubectl get secret -n tools grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)
 mysqlPwd=$(kubectl get secret --namespace db mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d)
-postgresqlPwd=$(kubectl get secret --namespace db postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+postgresqlPwd=$(kubectl get secret --namespace db postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 
 time=$(TZ=UTC date +"%Y-%m-%d %H:%M:%SZ")
 
@@ -40,7 +40,7 @@ div.xterm {
 			root pass: $mysqlPwd<br />
 			<br />
 			<div class="xterm">
-			kubectl run mysql-client --rm --tty -i --restart='Never' --image  docker.io/bitnami/mysql:8.0.31-debian-11-r10 --namespace db --env MYSQL_ROOT_PASSWORD=$mysqlPwd --command -- bash -c 'mysql -h mysql.db.svc.cluster.local -uroot -p"\$MYSQL_ROOT_PASSWORD"'
+			kubectl run mysql-client --rm --tty -i --restart='Never' --image  docker.io/bitnami/mysql:8.0.31-debian-11-r10 --namespace db --env MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace db mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d) --command -- bash -c 'mysql -h mysql.db.svc.cluster.local -uroot -p"\$MYSQL_ROOT_PASSWORD"'
 			</div>
 			OR
 			<div class="xterm">
@@ -54,7 +54,7 @@ div.xterm {
 			root pass: $postgresqlPwd<br />
 			<br />
 			<div class="xterm">
-			kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace db --image docker.io/bitnami/postgresql:11.13.0-debian-10-r0 --env="PGPASSWORD=\$(kubectl get secret --namespace db postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)" --command -- psql --host postgresql -U postgres -d postgres -p 5432
+			kubectl run postgresql-client --rm --tty -i --restart='Never' --namespace db --image docker.io/bitnami/postgresql:11.13.0-debian-10-r0 --env="PGPASSWORD=\$(kubectl get secret --namespace db postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)" --command -- psql --host postgresql -U postgres -d postgres -p 5432
 			</div>
 			OR
 			<div class="xterm">
