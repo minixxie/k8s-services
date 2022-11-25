0d2a5252a3be008548b1f25789d46e7149339252
Simon Xie <simon.xie@kuulabu.com>
2022-11-25 10:46:33 +0800

    Added kafka into hosts file

--- k8s/bin/gen-hosts.sh
+++ k8s/bin/gen-hosts.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cat <<EOF
-## Copy and paste this section into your /etc/hosts file:
+## Copy and paste the following 9 lines into your /etc/hosts file:
 $1  k8s.local
 $1  grafana.local
 $1  prometheus.local
 $1  mysql-exporter.local
 $1  mimir.local
 $1  kubeapps.local
+$1  kafka.local
 $1  kafka-ui.local
 $1  echo-server-api.local
 EOF
