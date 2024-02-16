89f455ff03786d4bf75a253dd19885fe0fdfcf01
Simon Tse <simon.tse.hk@gmail.com>
2024-02-16 14:13:01 +0800

    Added XXL Job as a dependency of java-springboot-svc

--- test-01-java-springboot.sh
+++ test-01-java-springboot.sh
@@ -14,5 +14,6 @@ make local-monitoring
 
 ### the application
 cd mysql && make local wait && cd -
-cd java-springboot-svc && make build && cd ./k8s/ && make up && cd .. && make wait && cd ../..
+cd xxl-job && make local wait && cd -
+cd java-springboot-svc && make build && cd ./k8s/ && make up && cd .. && make wait && cd ..
 cd java-springboot-svc/test && ./call-api.sh < Book.getBooks.http && cd -
