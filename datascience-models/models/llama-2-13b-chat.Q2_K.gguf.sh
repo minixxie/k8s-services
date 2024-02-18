e003d0276fafe9b8e531a52c5e70b07e415e78ee
Simon Tse <simon.tse.hk@gmail.com>
2024-02-18 17:00:13 +0800

    Added some more model files download script

---
+++ datascience-models/models/llama-2-13b-chat.Q2_K.gguf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -OsL "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q2_K.gguf"
---
+++ datascience-models/models/qwen1_5-7b-chat-q5_k_m.gguf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -OsL "https://huggingface.co/Qwen/Qwen1.5-7B-Chat-GGUF/resolve/main/qwen1_5-7b-chat-q5_k_m.gguf"
