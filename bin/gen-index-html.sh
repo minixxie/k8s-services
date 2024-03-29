1c5c6c2bdf16c49cf8cb4668416a720de31c9976
Simon Tse <simon.tse.hk@gmail.com>
2024-03-29 23:06:46 +0800

    fixed mysql's namespace to infra-mysql

--- bin/gen-index-html.sh
+++ bin/gen-index-html.sh
@@ -223,7 +223,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			root pass: $mysqlPwd<br />
 			<br />
 			<div class="xterm">
-			kubectl run mysql-client --rm --tty -i --restart='Never' --image  docker.io/bitnami/mysql:8.0.31-debian-11-r10 --namespace db --env MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace db mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d) --command -- bash -c 'mysql -h mysql.db.svc.cluster.local -uroot -p"\$MYSQL_ROOT_PASSWORD"'
+			kubectl run mysql-client --rm --tty -i --restart='Never' --image  docker.io/bitnami/mysql:8.0.31-debian-11-r10 --namespace db --env MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace db mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d) --command -- bash -c 'mysql -h mysql.infra-mysql.svc.cluster.local -uroot -p"\$MYSQL_ROOT_PASSWORD"'
 			</div>
 			OR
 			<div class="xterm">
--- xxl-job/init-db.sh
+++ xxl-job/init-db.sh
@@ -3,4 +3,4 @@
 kubectl run xxljob-init-db-script --rm --tty -i --restart='Never' --image docker.io/library/mysql:8.0.31-oracle --namespace infra-mysql \
 	--env MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace infra-mysql mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d) \
 	--command -- bash -c \
-	"cd /tmp; curl -O https://raw.githubusercontent.com/xuxueli/xxl-job/master/doc/db/tables_xxl_job.sql; mysql -h mysql.db.svc.cluster.local -u root -p\$MYSQL_ROOT_PASSWORD < ./tables_xxl_job.sql; mysql -h mysql.db.svc.cluster.local -uroot -p\$MYSQL_ROOT_PASSWORD -e \"CREATE USER xxl_job; GRANT ALL ON xxl_job.* TO 'xxl_job'@'%'; ALTER USER 'xxl_job'@'%' IDENTIFIED WITH mysql_native_password BY 'xxl_job'; FLUSH PRIVILEGES;\""
+	"cd /tmp; curl -O https://raw.githubusercontent.com/xuxueli/xxl-job/master/doc/db/tables_xxl_job.sql; mysql -h mysql.infra-mysql.svc.cluster.local -u root -p\$MYSQL_ROOT_PASSWORD < ./tables_xxl_job.sql; mysql -h mysql.infra-mysql.svc.cluster.local -uroot -p\$MYSQL_ROOT_PASSWORD -e \"CREATE USER xxl_job; GRANT ALL ON xxl_job.* TO 'xxl_job'@'%'; ALTER USER 'xxl_job'@'%' IDENTIFIED WITH mysql_native_password BY 'xxl_job'; FLUSH PRIVILEGES;\""
