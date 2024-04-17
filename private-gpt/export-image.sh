14130c198932e275a8877169b2e8ac9330f01ab9
Simon Tse <simon.tse.hk@gmail.com>
2024-04-17 23:08:01 +0800

    Made Makefile more generic to support both helm-chart folder or kustomize folder

--- private-gpt/export-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#tag=latest
-tag=0.2.0
-#nerdctl save 3x3cut0r/privategpt:$tag --output privategpt.$tag.tar --namespace=k8s.io
-
-tag=a79e02c36dedb0b981e6cfbcd16b4010cb3a909d
-nerdctl save minixxie/privategpt:$tag --output minixxie.privategpt.$tag.tar --namespace=k8s.io
--- private-gpt/import-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" ]; then
-	echo >&2 "usage: $0 ./minixxie.privategpt.tag.tar"
-	exit 1
-fi
-nerdctl load -i "$1" --namespace=k8s.io
