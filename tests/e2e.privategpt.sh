4ed191d12b2a814ca47ea9a7b753430468a3d433
Simon Tse <simon.tse.hk@gmail.com>
2024-05-05 12:28:26 +0800

    Changed back appID to app

--- tests/e2e.privategpt.sh
+++ tests/e2e.privategpt.sh
@@ -10,10 +10,5 @@ make -s k8s-redo
 ### the application
 make -s -C ./datascience-models local wait test
 make -s -C ./qdrant local wait test
-make -s -C ./mongodb local wait test
-cd private-gpt
-if [ -f ./minixxie.privategpt.a79e02c36dedb0b981e6cfbcd16b4010cb3a909d.tar ]; then
-	./import-image.sh minixxie.privategpt.a79e02c36dedb0b981e6cfbcd16b4010cb3a909d.tar
-fi
-cd -
-make -s -C ./private-gpt local wait test
+make -s -C ./infra-mongodb@7.0.5 local wait test
+make -s -C ./ai-private-gpt local wait test
