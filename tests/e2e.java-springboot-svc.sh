72a33bd9999404322c692dc291d4154cc8f28d3a
Simon Tse <simon.tse.hk@gmail.com>
2024-05-24 00:32:58 +0800

    fixed the e2e test script for java-springboot-svc

--- tests/e2e.java-springboot-svc.sh
+++ tests/e2e.java-springboot-svc.sh
@@ -11,11 +11,11 @@ make -s k8s-redo
 make local-monitoring
 
 ### Application's dependencies
-make -s -C ./infra-mysql@8.2.0 local wait test
-make -s -C ./xxl-job local wait test
+cd infra-mysql@8.2.0 && ./local && ./test.sh && cd ..
+cd infra-xxljob && ./local && ./test.sh && cd ..
 
 ### Application
-cd java-springboot-svc && make build && cd ./k8s/ && make up && cd .. && make wait && cd ..
+cd java-springboot-svc && make build && make up get wait && cd ..
 #cd java-springboot-svc/test && ./call-api.sh < Book.getBooks.http && cd -
 
 ### TESTS
