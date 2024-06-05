3f858410b7d8e067b81ebfe7aeb7012413bd92cb
Simon Tse <simon.tse.hk@gmail.com>
2024-06-06 00:14:47 +0800

    Revamped redis-cluster with new Makefile

---
+++ tests/e2e.redis-cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+scriptPath=$(cd $(dirname "$0") && pwd)
+cd "$scriptPath"/..
+
+### k8s nodes
+make -s k8s-redo
+
+### Application
+make -s -C infra-redis-cluster img local wait test
+
+### TESTS
+
+
+echo SUCCESS
