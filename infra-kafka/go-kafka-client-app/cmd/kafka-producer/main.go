c9a39e9c47f490760901852ad0615497e34c102e
Simon Tse <simon.tse.hk@gmail.com>
2024-08-10 21:34:27 +0800

    Changed kafka message to JSON string

--- infra-kafka/go-kafka-client-app/cmd/kafka-producer/main.go
+++ infra-kafka/go-kafka-client-app/cmd/kafka-producer/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/IBM/sarama"
 )
+
+type Message struct {
+	Greeting string `json:"greeting"`
+}
 
 func main() {
 	// Set up Kafka producer configuration
@@ -19,11 +24,19 @@ func main() {
 	}
 	defer producer.Close()
 
+	// Create a message struct and marshal it to JSON
+	msg := Message{Greeting: "Hello, Kafka!"}
+	jsonValue, err := json.Marshal(msg)
+	if err != nil {
+		log.Fatalf("Failed to marshal message to JSON: %v", err)
+	}
+
 	// Send a test message to Kafka topic
 	topic := "test-topic"
 	message := &sarama.ProducerMessage{
 		Topic: topic,
-		Value: sarama.StringEncoder("Hello, Kafka!"),
+		Key:   sarama.StringEncoder("Greetings"),
+		Value: sarama.StringEncoder(jsonValue),
 	}
 
 	partition, offset, err := producer.SendMessage(message)
