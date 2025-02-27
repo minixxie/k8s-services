aa725d0ccd0bcc7919f7dcef1a068d23a1c53df2
Simon Tse <simon.tse.hk@gmail.com>
2025-02-27 09:54:37 +0800

    Improved the model download scripts

--- datascience-models/models/DeepSeek-V2-Lite-Chat-GGUF.sh
+++ datascience-models/models/DeepSeek-V2-Lite-Chat-GGUF.sh
@@ -6,5 +6,5 @@ scriptPath=$(cd $(dirname "$0") && pwd)
 cd "$scriptPath"
 mkdir -p DeepSeek-V2-Lite-Chat-GGUF
 cd DeepSeek-V2-Lite-Chat-GGUF
-wget https://huggingface.co/mradermacher/DeepSeek-V2-Lite-Chat-GGUF/resolve/main/DeepSeek-V2-Lite-Chat.Q4_K_M.gguf
+wget -c https://huggingface.co/mradermacher/DeepSeek-V2-Lite-Chat-GGUF/resolve/main/DeepSeek-V2-Lite-Chat.Q4_K_M.gguf
 
--- datascience-models/models/DeepSeek-V3-GGUF.sh
+++ datascience-models/models/DeepSeek-V3-GGUF.sh
@@ -7,5 +7,5 @@ cd "$scriptPath"
 mkdir -p DeepSeek-V3-GGUF
 cd DeepSeek-V3-GGUF
 for i in $(seq -w 1 9); do
-	wget https://huggingface.co/unsloth/DeepSeek-V3-GGUF/resolve/main/DeepSeek-V3-Q4_K_M/DeepSeek-V3-Q4_K_M-0000${i}-of-00009.gguf
+	wget -c https://huggingface.co/unsloth/DeepSeek-V3-GGUF/resolve/main/DeepSeek-V3-Q4_K_M/DeepSeek-V3-Q4_K_M-0000${i}-of-00009.gguf
 done
