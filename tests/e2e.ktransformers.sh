525e1395d3bc8d5bbd898558cd19ac989c40c2eb
Simon Tse <simon.tse.hk@gmail.com>
2025-02-27 10:34:12 +0800

    Made DeepSeek-V2-Lite the default model for ktransformers, as my computer can't be sufficient to run V3

--- tests/e2e.ktransformers.sh
+++ tests/e2e.ktransformers.sh
@@ -11,8 +11,10 @@ make -s k8s-redo
 make -s -C ./nvidia-gpu-operator img local wait test
 
 ### the application
-./datascience-models/models/DeepSeek-V3.sh
-./datascience-models/models/DeepSeek-V3-GGUF.sh
+#./datascience-models/models/DeepSeek-V3.sh
+./datascience-models/models/DeepSeek-V2-Lite.sh
+#./datascience-models/models/DeepSeek-V3-GGUF.sh
+./datascience-models/models/DeepSeek-V2-Lite-Chat-GGUF.sh
 make -s -C ./datascience-models local wait test
 
 make -s -C ./ai-ktransformers img local wait test
