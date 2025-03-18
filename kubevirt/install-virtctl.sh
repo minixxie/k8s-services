73b101d175f94f53a2900c97f7d52ba0b468c9ed
Simon Tse <simon.tse.hk@gmail.com>
2025-03-18 16:40:47 +0800

    Added kubevirt

---
+++ kubevirt/install-virtctl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://kubevirt.io/quickstart_cloud/
+
+VERSION=$(kubectl get kubevirt.kubevirt.io/kubevirt -n kubevirt -o=jsonpath="{.status.observedKubeVirtVersion}")
+ARCH=$(uname -s | tr A-Z a-z)-$(uname -m | sed 's/x86_64/amd64/') || windows-amd64.exe
+echo ${ARCH}
+curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-${ARCH}
+chmod +x virtctl
+sudo install virtctl /usr/local/bin
